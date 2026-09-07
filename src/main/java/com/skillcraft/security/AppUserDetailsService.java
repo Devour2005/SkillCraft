3529028df106ae30465662d05f135145fd95c1cf
a.kuidin <devour2035@gmail.com>
2026-09-07 16:24:39 +0300

    Add initial security configuration + JWT

---
+++ src/main/java/com/skillcraft/security/AppUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.skillcraft.security;
+
+import com.skillcraft.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AppUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository
+                .findByEmailIgnoreCase(email)
+                .map(AppUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
