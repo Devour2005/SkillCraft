ad386b77d79982d1b9c4febce32473e4f49a5e60
a.kuidin <devour2035@gmail.com>
2026-09-08 16:55:39 +0300

    Add tests with testcontainers

--- management/src/test/java/com/skillcraft/SkillCraftApplicationTests.java
+++ management/src/test/java/com/skillcraft/SkillCraftApplicationTests.java
@@ -3,9 +3,11 @@ package com.skillcraft;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestPropertySource;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
+@TestPropertySource(properties = "jwt.secret=test-only-secret-not-used-outside-the-test-suite-0123456789")
 class SkillCraftApplicationTests {
 
 	@Test
